7f8710ad7d8b591ee485e01a6d4c1b772488e384
ZhannaLytvynenko <zvlitvinenko01@gmail.com>
2020-01-06 01:20:28 +0200

    add class Program

--- src/Handler/ImageCoder.kt
+++ src/Handler/ImageCoder.kt
@@ -13,8 +13,8 @@ class ImageCoder(private var mode: String = "enc", private var password: String 
 
     override fun code(image: BufferedImage): Array<Any> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        val codeResult = CodeResult.NONE
-        val codedImage = image
+//        val codeResult = CodeResult.NONE
+//        val codedImage = image
 
 //        val ivspec = IvParameterSpec(password.toByteArray())
 //        val key = SecretKeySpec(password.toByteArray(), algorithm)
@@ -25,28 +25,15 @@ class ImageCoder(private var mode: String = "enc", private var password: String 
         
 //    val encryptedBase64 = Base64.getEncoder().encode(cipher.doFinal(fileContent))
 
-        return arrayOf(codedImage, codeResult)
+//        return arrayOf(codedImage, codeResult)
     }
 
     override fun code(listOfImages: List<BufferedImage>): Array<Any> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        var codeResult = CodeResult.NONE
-        var resultList = listOfImages
+//        var codeResult = CodeResult.NONE
+//        var resultList = listOfImages
 
-<<<<<<< HEAD
-class ImageCoder(var mode: String = "enc", var password: String = "00000000") {
-//    fun Code(image: BufferedImage): BufferedImage{
-//        var tmp = 1
-//        for(i in image.minX..image.width.minus(1)) {
-//            for (j in image.minY..image.height.minus(1)) {
-//                image.setRGB(i, j, tmp * image.getRGB(i, j).plus(password.toInt()))
-//                tmp *= -1
-//            }
-//        }
-//       return image
-}
-=======
-        return arrayOf(resultList, codeResult)
+
+//        return arrayOf(resultList, codeResult)
     }
 }
->>>>>>> IMGCoder
--- src/Handler/Main.kt
+++ src/Handler/Main.kt
@@ -1,41 +1,61 @@
 package Handler
 
-<<<<<<< HEAD
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
 fun help(){
     println("There are such commands:\n" +
-            "/path [directory_path] - the directory where picture(s) is(are)\n" +
-            "/path /f [image_path] - the full path to one image\n" +
-            "/enc [password] - to encode picture by password\n" +
-            "/dec [password] - to decode picture by password\n" +
+            "/path_[directory path] - the directory where picture(s) is(are)\n" +
+//            "/path /f [image_path] - the full path to one image\n" +
+            "/enc_[password] - to encode picture by password\n" +
+            "/dec_[password] - to decode picture by password\n" +
             "/ex - to exit")
 }
-fun commandReader():List<String> = readLine().toString().split(' ')
-
+fun commandReader():List<String> = readLine().toString().split('_')
+fun printResultInfo(codeResult: CodeResult){
+    println(when(codeResult){
+        CodeResult.SUCCESS -> "Done successfully."
+        CodeResult.WRONG_DECRYPT_KEY -> "You have entered wrong key."
+        CodeResult.WRONG_KEY_LENGTH -> "The key must have 16 symbols."
+    })
+}
 fun menu(){
     var pictures = mutableListOf<BufferedImage>()
     var changedPictures = mutableListOf<BufferedImage>()
-    //TODO create loop
-    //TODO tell user what to do and how call help
-    when(commandReader()[0]){
-        "/?" -> help()
-        "/path" -> //TODO check dir or file
-            File(commandReader()[1]).forEachLine { pictures.add(ImageIO.read(File(it))) }
-//        "/enc" -> {
-//            for(element in pictures){
-//               changedPictures.add(coder.Code(element))
-//            }
-//        }
-        "/dec"-> {
-
-        }
-        "/ex" -> return
-        else -> {
-            println("Wrong command.")
-            menu()
+    var path: String = ""
+    println("/? - help")
+    while(true){
+        val input = commandReader()
+        when(input[0]){
+            "/?" -> help()
+            "/path" -> {
+                path = input[1]
+                Runtime.getRuntime().exec("cmd /c cd directory check\\ && start findImg.bat \"${path}\"")
+                File("directory check\\file.txt").forEachLine { pictures.add(ImageIO.read(File("${path}\\${it}"))) }
+            }
+            "/enc" -> {
+                var coder = ImageCoder("enc", input[1])
+                var result = coder.code(pictures) //list of coded images and result
+                changedPictures = result[0]
+                for(i in changedPictures.indices){
+                    File(path).writeBytes(changedPictures[i] as ByteArray)
+                }
+                printResultInfo(result[1])
+            }
+            "/dec"-> {
+                var decoder = ImageCoder("dec", input[1])
+                var result = decoder.code(pictures)
+                changedPictures = result[0]
+                for(i in changedPictures.indices){
+                    File(path).writeBytes(changedPictures[i] as ByteArray)
+                }
+                printResultInfo(result[1])
+            }
+            "/ex" -> return
+            else -> {
+                println("Wrong command.")
+            }
         }
     }
 }
